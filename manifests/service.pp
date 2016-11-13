22cb0386914bfbb55e655326a5b7814617095290
Flannon <flannon@5eight5.com>
2016-11-13 16:48:06 -0500

    Added simple service script and service start definition.  iCurrenlty the service script has no status and only starts tomcat/fedora.  I still need to write a complete service script.

---
+++ manifests/service.pp
@@ -0,0 +1,28 @@
+#
+class fedora_repository::service (
+
+) inherits fedora_repository::params {
+
+  # install the service script
+  file { "/etc/init.d/${::user}_tomcat" :
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    content => template("${module_name}/fedora_tomcat.erb"),
+    require => Class["${module_name}::install"],
+  }
+
+  service { "${::user}_tomcat" :
+    ensure     => 'running',
+    enable     => true,
+    hasstatus  => false,
+    hasrestart => false,
+    provider   => 'redhat',
+    require    => [Class["${module_name}::install"],
+                  File["/etc/init.d/${::user}_tomcat"]],
+    #require    => [File['/etc/init.d/tomcat'],
+    #    File["${install_dir}/.setup-database.complete"]],
+  }
+
+}
