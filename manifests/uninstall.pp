3dbf242a9b059361a2001ed747bb6ca249aa08a0
Flannon <flannon@5eight5.com>
2016-11-13 16:37:05 -0500

    Uninstalling the user to remove the installtion directory works fine, but I still need to sort out removing the service script and the installation package files.

--- manifests/uninstall.pp
+++ manifests/uninstall.pp
@@ -10,10 +10,29 @@
 #
 # Copyright 2016 Your name here, unless otherwise noted.
 #
-class fedora_repository::uninstall {
+class fedora_repository::uninstall (
+  $artifact_id = $fedora_repository::params::artifact_id,
+  $stage_dir   = $fedora_repository::params::stage_dir,
+  $version     = $fedora_repository::params::version,
+) inherits fedora_repository::params {
 
   class { fedora_repository :
     installed => absent,
   }
+
+  #file { "${stage_dir}/install.properties remove" :
+  #  ensure => absent,
+  #  path   => "${stage_dir}/install.properties",
+  #}
+
+  #file { "${stage_dir}/${artifact_id}-${version}.jar remove" :
+  #  ensure => absent,
+  #  path   => "${stage_dir}/${artifact_id}-${version}.jar",
+  #}
+
+  #file { '/etc/init.d/fedora_tomcat remove' :
+  #  ensure => absent,
+  #  path   => '/etc/init.d/fedora_tomcat',
+  #}
 
 }
