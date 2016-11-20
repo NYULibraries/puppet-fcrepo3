53f6f060b09195684bfe7a12803ab29d78fc7eef
Flannon <flannon@5eight5.com>
2016-11-20 11:58:38 -0500

    Got rid of the include statements and added parameterized class definitions.

--- manifests/init.pp
+++ manifests/init.pp
@@ -21,16 +21,22 @@
 # Copyright 2016 Your name here, unless otherwise noted.
 #
 class fedora_repository (
-  $installed = $fedora_repository::params::installed,
+  $installed    = $fedora_repository::params::installed,
   $nexus_server = $fedora_repository::params::nexus_server,
-  $version = $fedora_repository::params::version,
+  $nexus_port   = $fedora_repository::params::nexus_port,
+  $user         = $fedora_repository::params::user,
+  $version      = $fedora_repository::params::version,
 ) inherits fedora_repository::params {
 
-  #class { fedora_repository::user :
-  #  installed => $installed,
+  class { 'fedora_repository::install' :
+    group_id     => $group_id,
+    artifact_id  => $artifact_id,
+    version      => $version,
+    nexus_server => $nexus_server,
+    nexus_port   => $nexus_port,
+  }
+  #class { 'fedora_repository::service' :
+  #  user => $user,
   #}
-  include fedora_repository::user
-  include fedora_repository::install
-  #include fedora_repository::service
 
 }
